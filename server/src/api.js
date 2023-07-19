ed4c26ce47459d2ee0dcd6f5e3fe800ec3f30fb8
nsaimk <103388334+nsaimk@users.noreply.github.com>
2023-07-19 18:33:21 +0100

    serverless

---
+++ server/src/api.js
@@ -0,0 +1,35 @@
+const express = require("express");
+
+const serverless = require("serverless-http");
+
+const app = express();
+
+const router = express.Router();
+
+async function connectToDb() {
+  const client = new MongoClient(connectionString);
+  try {
+    conn = await client.connect();
+  } catch (e) {
+    console.error(e);
+  }
+  return conn.db("cyf-fullstack-product");
+}
+
+router.get("/pro", async (req, res) => {
+  connectToDb()
+    .then((db) => db.collection("cyf-fullstack-product"))
+    .then(async (collection) => {
+      const products = await collection.find({}).toArray();
+      console.log(products);
+
+      res.json(products);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});
+
+app.use("/", router);
+
+module.exports.handler = serverless(app);
